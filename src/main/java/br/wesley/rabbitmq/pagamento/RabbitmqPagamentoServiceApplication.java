124801a93981cac7a58a372f481b58f8134454c8
wesleymrosa <wesleymrosa@gmail.com>
2025-05-21 11:09:27 -0300

    Projeto rabbitmq-pagamento-service criado via Spring Initializr para consumir mensagens da fila de pagamento

---
+++ src/main/java/br/wesley/rabbitmq/pagamento/RabbitmqPagamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.rabbitmq.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPagamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPagamentoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/wesley/rabbitmq/pagamento/RabbitmqPagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.rabbitmq.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
